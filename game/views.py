9ea098426709a1eedaf6900f733a4109b57a786c
Aleksander Kozminski <alek.k@berkeley.edu>
2021-07-23 15:15:56 -0700

    rename draw->game

--- game/views.py
+++ game/views.py
@@ -2,9 +2,9 @@
 from django.shortcuts import render
 
 def index(request):
-    return render(request, 'draw/index.html')
+    return render(request, 'game/index.html')
 
 def room(request, room_name):
-    return render(request, 'draw/room.html', {
+    return render(request, 'game/room.html', {
         'room_name': room_name
     })
